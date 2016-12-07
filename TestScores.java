33300c0e87319aa97350e3cfebd5a11d413dde2e
NovaStorm80 <sithclan01128@hotmail.com>
2016-12-07 10:16:13 -0600

    Add files via upload

---
+++ TestScores.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.text.*;
+public class TestScores
+{
+	public static void main(String[] arg)
+	{
+		//----  declare vars --------
+		String  ans = "";
+
+		int  total      = 0,
+			 score      = 0,
+			 scoreHigh  = 0,
+			 scoreLow   = 0,
+			 scoreAcum  = 0;
+
+	    double scoreAvg = 0;
+
+	    boolean performTask = true
+
+		Scanner keyboard = new Scanner(System.in);
+
+		DecimalFormat df1 = new DecimalFormat ("##0.0");
+
+		//-------- start up routine-------------
+		System.out.println(" * * * Welcome to Test Scores * * *");
+
+			//----------- starting my task ---------------------
+			while (performTask)
+			{
+				System.out.print("Begin entering test scores <when done, enter -1> :");
+				score = keyboard.nextInt();
+
+				while(score >= 0 )
+						{
+							total += score;
+							score = keyboard.nextInt();
+							scoreAcum ++;
+						}//  end SENTINEL control
+			//---------- Calculations
+			scoreAvg = total / cntr;
+
+			//---------- End Task
+			System.out.println("\nNum of Scores:          " + scoreAcum);
+			System.out.println("Average Score:          " + df1.format(scoreAvg));
+			System.out.println("Lowest Score:            " +  scoreLow);
+			System.out.println("Highest Score:          " +  scoreHigh);
+
+			//--------------step 3:  update control var -----------
+			System.out.print("\nWould you like to process more tests(y/n)?");
+			ans = keyboard.next();
+
+			if (ans.equalsIgnoreCase("y") )
+							System.out.println("==================================="
+							+ "===================================");
+						else
+				performTask = false;
+			}// end loop
+
+
+
+		} // end main
+}// end pgm
